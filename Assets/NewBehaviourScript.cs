c0e3149ec8cd58bff226755d9357beddddd97120
jzz123 <737288022@qq.com>
2021-10-14 16:09:34 +0800

    testPackage

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Application.temporaryCachePath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Update");
+        print("Update");
+    }
+    private void FixedUpdate()
+    {
+        //Debug.Log("FixedUpdate");
+        print("FixedUpdate");
+    }
+    private void LateUpdate()
+    {
+        print("LateUpdate");
+    }
+}
